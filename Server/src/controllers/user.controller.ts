21a0607f13ba45564d4e962ff1421a36a5b718e5
Erick Developer <erick.developer.8@gmail.com>
2024-04-20 00:05:46 -0600

    feat<backend>: adding the delete functionality to the user controller and the respective route

--- Server/src/controllers/user.controller.ts
+++ Server/src/controllers/user.controller.ts
@@ -66,4 +66,18 @@ const updateUser = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-export { showUsers, insertUser, authUsers, updateUser };
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const user = await User.findByIdAndDelete(
+            req.params.id
+        )
+        if (!user) {
+            res.status(404).json({ message: 'User dont found' });
+        }
+        res.status(200).json({ message: 'User deleted correctly' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export { insertUser, showUsers, authUsers, updateUser, deleteUser };
--- Server/src/routes/user.routes.ts
+++ Server/src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { showUsers, insertUser, authUsers, updateUser } from "../controllers/user.controller";
+import { showUsers, insertUser, authUsers, updateUser, deleteUser} from "../controllers/user.controller";
 import { authToken } from "../middlewares/auth.token";
 const router = Router();
 
 router.get('/users', showUsers);
 router.post('/singUp', insertUser);
 router.post('/singIn', authUsers);
-router.put('/updateUser/:id', updateUser);
+router.put('/updateUser/:id', authToken, updateUser);
+router.delete('/deleteUser/:id', authToken, deleteUser);
 
 export default router;
