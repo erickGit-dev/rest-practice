c0f8d58105a1d9d5073ad10fb53b3038cb95146d
Erick Developer <erick.developer.8@gmail.com>
2024-05-12 23:41:45 -0600

    refator<frontend>: changing the css.modules vars names

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,26 +1,35 @@
 import React from "react";
+import { FaBars, FaX } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import style from "../styles/navbar.module.css";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <Link to="/" className={style['app-name']}>Cer0</Link>
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contacts">Contact</Link>
-        </li>
-        <li>
-          <Link to="/singup">Sing Up</Link>
-        </li>
-        <li>
-          <Link to="/login">Log In</Link>
-        </li>
-      </ul>
-    </nav >
+    <header>
+      <nav>
+        <Link to="/" className={style['app-name']}>Cer0</Link>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contact</Link>
+          </li>
+          <li>
+            <Link to="/singup">Sing Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+        <button>
+          <FaBars />
+        </button>
+      </nav >
+      <button>
+        <FaX />
+      </button>
+    </header>
   );
 }
 
