35fa694843be9c2cbf12a327182897701fdca62f
Erick Developer <erick.developer.8@gmail.com>
2024-05-23 16:07:29 -0600

    feat<frontend>: adding style to the login page

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { FaBars, FaX } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import style from "../styles/navbar.module.css";
+import Style from "../styles/navbar.module.css";
 
 const Navbar: React.FC = () => {
   const navRef = React.useRef<HTMLDivElement>(null);
 
   const toggleNav = () => {
-   navRef.current?.classList.toggle(style['nav-toggle']);
+    navRef.current?.classList.toggle(Style['nav-toggle']);
   }
 
   return (
     <header>
-      <Link to="/" className={style['app-name']}>Cer0</Link>
+      <Link to="/" className={Style['app-name']}>Cer0</Link>
       <nav ref={navRef}>
         <Link to="/about" onClick={toggleNav}>About</Link>
         <Link to="/contacts" onClick={toggleNav}>Contacts</Link>
-        <Link to="/singup" onClick={toggleNav} className={style['sing-up']}>Sing Up </Link>
-        <Link to="/login" onClick={toggleNav} className={style['log-in']} >Log In</Link>
-        <button className={style['menu-close']} onClick={toggleNav}>
+        <Link to="/login" onClick={toggleNav} className={Style['log-in']} >Log In</Link>
+        <Link to="/singup" onClick={toggleNav} className={Style['sing-up']}>Sing Up </Link>
+        <button className={Style['menu-close']} onClick={toggleNav}>
           <FaX size={'1.3em'} />
         </button>
       </nav >
-      <button className={style['menu-bars']} onClick={toggleNav}>
+      <button className={Style['menu-bars']} onClick={toggleNav}>
         <FaBars size={'1.3em'} />
       </button>
     </header>
--- client/src/components/pages/singup.tsx
+++ client/src/components/pages/singup.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Style from "../../styles/singup.module.css"
+import { Link } from "react-router-dom";
 
 const Singup: React.FC = () => {
-    return (
-      <div>Singup</div>
-    );
+  return (
+    <div className={Style['sing-up']}>
+      <Link to="/" className={Style['app-name']}>Cer0</Link>
+      <div className={Style['card']}>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sing Up</button>
+        </div>
+    </div>
+  );
 }
 
 export default Singup;
